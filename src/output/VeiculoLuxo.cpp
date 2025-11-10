984cce85d1725c8b9e47298d9a34dc25f511f66f
Jamile Fonseca <jamilefonsecaadm@gmail.com>
2025-11-10 17:19:30 -0300

    Adicionando a implementacao VeiculoLuxo.cpp

---
+++ src/output/VeiculoLuxo.cpp
@@ -0,0 +1,25 @@
+#include "VeiculoLuxo.hpp"
+#include <iostream>
+
+const std::map<std::string, double> VeiculoLuxo::_tabelaDePrecos = {
+    {"Ferrari",      4500.00}, 
+    {"BMW",          3500.00},
+    {"Porsche",      4450.00},
+    {"Ranger Rover", 3200.00},
+    {"Mercedes-Benz",3000.00}
+};
+
+VeiculoLuxo::VeiculoLuxo(const std::string& placa, const std::string& marca, 
+    const std::string& modelo, int ano) : Veiculo(placa, marca, modelo, ano, "Luxo"){
+
+    }
+
+double VeiculoEconomico::getPrecoDiaria() const override{
+    std::string meuModelo = this->getModelo();
+    auto it = _tabelaDePrecos.find(meuModelo);
+
+    if(it != _tabelaDePrecos.end()){
+        return it -> second;
+    }
+
+}
